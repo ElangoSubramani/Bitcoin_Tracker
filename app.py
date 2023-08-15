5d6c4e952914db8a15ac3055b9389de2c6e15b05
ELANGO S <122277416+ElangoSubramani@users.noreply.github.com>
2023-08-15 22:05:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
 server=app.server
 
 coins = ["bitcoin", "ethereum", "binancecoin", "ripple"]
-interval = 3000  
+interval = 9000  
 api_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 
 def make_card(coin):
