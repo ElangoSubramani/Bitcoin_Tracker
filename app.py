6ac512fcf81f19b540fc81ce231ee8c6fa8a595c
ELANGO S <122277416+ElangoSubramani@users.noreply.github.com>
2023-08-15 14:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
                 meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
-app=app.sever
+server=app.server
 
 coins = ["bitcoin", "ethereum", "binancecoin", "ripple"]
 interval = 3000  
